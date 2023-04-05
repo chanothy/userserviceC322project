36ff511bcea3cba85b575249765c141ff4973290
timchan <timchan@iu.edu>
2023-04-05 19:18:34 -0400

    Initial commit

---
+++ src/main/java/edu/iu/c322/userserviceC322project/UserserviceC322projectApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.userserviceC322project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserserviceC322projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserserviceC322projectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/iu/c322/userserviceC322project/UserserviceC322projectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.userserviceC322project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceC322projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
